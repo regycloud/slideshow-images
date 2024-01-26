709af131eb7a331d2bbb0f0d09856fb330e6d56d
regycloud <regycloud@yahoo.com>
2024-01-26 11:08:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 // JSX
 import BasicSlider from "./components/BasicSlider/BasicSlider";
-import VerticalSlider from "./components/VerticalSlider/VerticalSlider";
-import ZoomSlider from "./components/ZoomSlider/ZoomSlider";
-import SlidesChildrenSlider from "./components/SlidesChildrenSlider/SlidesChildrenSlider";
-import BlendModeSlider from "./components/BlendModeSlider/BlendModeSlider";
-import NavbarSlider from "./components/NavbarSlider/NavbarSlider";
-import ButtonsSlider from "./components/ButtonsSlider/ButtonsSlider";
-import AutoplayButtonSlider from "./components/AutoplayButtonSlider/AutoplayButtonSlider";
+// import VerticalSlider from "./components/VerticalSlider/VerticalSlider";
+// import ZoomSlider from "./components/ZoomSlider/ZoomSlider";
+// import SlidesChildrenSlider from "./components/SlidesChildrenSlider/SlidesChildrenSlider";
+// import BlendModeSlider from "./components/BlendModeSlider/BlendModeSlider";
+// import NavbarSlider from "./components/NavbarSlider/NavbarSlider";
+// import ButtonsSlider from "./components/ButtonsSlider/ButtonsSlider";
+// import AutoplayButtonSlider from "./components/AutoplayButtonSlider/AutoplayButtonSlider";
 
 const app = () => {
   return (
